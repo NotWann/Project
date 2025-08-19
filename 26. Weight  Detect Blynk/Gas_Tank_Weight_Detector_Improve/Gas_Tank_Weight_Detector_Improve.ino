3bebe38cd2ffb693c2ab5af42ac2caeeadcc2dae
NotWann <ikhwankhusairi42@gmail.com>
2025-08-19 11:04:22 +0800

    Add files via upload

---
+++ 26. Weight  Detect Blynk/Gas_Tank_Weight_Detector_Improve/Gas_Tank_Weight_Detector_Improve.ino
@@ -0,0 +1,300 @@
+#define BLYNK_PRINT Serial
+#include <BlynkSimpleEsp32.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+
+char auth[] = "YFH-6ajcrFkyOmauW1_8UcYbJioGvEr9";  // Auth token
+char ssid[] = "abcd";                              // WiFi ssid
+char pass[] = "123456789";                         // WiFi password
+
+char server[] = "prakitblog.com";                  // Custom server
+int port = 8181;                                   // port
+
+/*################################################################*/
+#include <UniversalTelegramBot.h>
+#include <ArduinoJson.h>
+#define BOT_TOKEN "8209938136:AAF_JLVbsy2fl4JspVI31So3K_JPvVgmM9w"  // Your BotFather token
+#define CHAT_ID "1481559225"                                        // Your Telegram ID
+WiFiClientSecure secured_client;
+UniversalTelegramBot bot(BOT_TOKEN, secured_client);
+
+/*################################################################*/
+#include <HX711_ADC.h>
+const int HX711_dout = 4;  //mcu > HX711 dout pin
+const int HX711_sck = 5;   //mcu > HX711 sck pin
+HX711_ADC LoadCell(HX711_dout, HX711_sck);
+
+/*################################################################*/
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+/*################################################################*/
+#define FILTER_SAMPLES 10  // number of readings to average
+
+/*################################################################*/
+/* Pinout */
+/*################################################################*/
+const int buzzer_pin = 12;
+const int led_pin = 2;
+const int lpg_analog_pin = 34;
+const int lpg_digital_pin = 23;
+
+/*################################################################*/
+/* Variable for HX711 weight sensor */
+/*################################################################*/
+const float calibrationValue = -28.53;
+unsigned long t = 0;
+
+int flagWeight = 0;
+float set_weight = 10.00;
+
+/*################################################################*/
+/* Variable for LPG Sensor action */
+/*################################################################*/
+int lock_lpg = 0;
+
+/*################################################################*/
+/* Telegram */
+/*################################################################*/
+unsigned long lastMessageSent = 0;           // Timestamp of last Telegram message
+const unsigned long messageCooldown = 1000;  // 1000ms cooldown for messages
+
+/*################################################################*/
+/* Function to read weight */
+/*################################################################*/
+void read_weight() {
+  static boolean newDataReady = 0;
+  const int serialPrintInterval = 20;
+
+  /******************************************************************/
+  if (LoadCell.update()) newDataReady = true;
+  /******************************************************************/
+
+  /******************************************************************/
+  if (newDataReady) {
+    if (millis() > t + serialPrintInterval) {
+      float weight = LoadCell.getData();
+
+      /******************************************************************/
+      weight = abs(weight) / 1000;  // Convert g to kg
+      /******************************************************************/
+
+      lcd.setCursor(0, 0);
+      lcd.print("Weight:");
+      lcd.print(weight, 2);
+      lcd.print(" kg ");
+
+      lcd.setCursor(0, 1);
+      lcd.print("Set:");
+      lcd.print(set_weight, 2);
+      lcd.print(" kg");
+
+      // Display blynk
+      Blynk.virtualWrite(V0, weight);
+
+      Serial.print("Weight: ");
+      Serial.print(weight, 2);
+      Serial.println(" kg ");
+
+      newDataReady = 0;
+      t = millis();
+
+      /******************************************************************/
+      // Check if the read weight is less than set weight
+      /******************************************************************/
+      if (weight < set_weight && weight > 0.99 && flagWeight == 0) {
+        digitalWrite(buzzer_pin, HIGH);
+        digitalWrite(led_pin, HIGH);
+        Blynk.email("2xlukman25@gmail.com", "Alert", "Weight Detected!");  // Send Alert to email
+        bot.sendMessage(CHAT_ID, "Alert: Weight Reach!", "");              // Send Alert to Telegram
+        Serial.println("Weight Reach!");
+
+        flagWeight = 1;
+      } else if (weight > set_weight && flagWeight == 1) {
+        digitalWrite(buzzer_pin, LOW);
+        digitalWrite(led_pin, LOW);
+
+        flagWeight = 0;
+      }
+      /******************************************************************/
+      // Prevent false alert if the weight is less than 1 kg
+      /******************************************************************/
+      else if (weight < 1.00) {
+        digitalWrite(buzzer_pin, LOW);
+        digitalWrite(led_pin, LOW);
+        flagWeight = 0;
+      }
+    }
+  }
+  /******************************************************************/
+}
+
+/*################################################################*/
+/* Function for LPG MQ2 gas sensor*/
+/*################################################################*/
+void read_gas() {
+  static int readings[FILTER_SAMPLES];
+  static int index = 0;
+  static long sum = 0;
+  static int count = 0;
+
+  int gas_analog_read = analogRead(lpg_analog_pin);
+  int gas_digital_read = digitalRead(lpg_digital_pin);
+
+  /******************************************************************/
+  sum -= readings[index];
+  readings[index] = gas_analog_read;
+  sum += gas_analog_read;
+  index = (index + 1) % FILTER_SAMPLES;
+  /******************************************************************/
+
+  if (count < FILTER_SAMPLES) count++;
+  int avg_gas = sum / count;
+
+  /******************************************************************/
+  // Adjust these after testing
+  const int gasThreshold = 3200;  // Calibrate this
+  /******************************************************************/
+
+  /******************************************************************/
+  if (avg_gas > gasThreshold) {
+    digitalWrite(buzzer_pin, HIGH);
+    digitalWrite(led_pin, HIGH);
+
+    /******************************************************************/
+    if (lock_lpg == 0 && (millis() - lastMessageSent >= messageCooldown)) {
+      Serial.println("Gas Detected!");
+      bot.sendMessage(CHAT_ID, "Alert: LPG Gas Detected!", "");           //Send Alert to Telegram
+
+      lastMessageSent = millis();
+      lock_lpg = 1;
+    }
+    /******************************************************************/
+  }
+  /******************************************************************/ 
+  else {
+    // No gas — reset
+    if (lock_lpg == 1) {
+      Serial.println("Gas cleared.");
+    }
+    lock_lpg = 0;
+    digitalWrite(buzzer_pin, LOW);
+    digitalWrite(led_pin, LOW);
+  }
+  /******************************************************************/
+
+  Blynk.virtualWrite(V3, avg_gas);
+  Serial.printf("LPG raw: %d | avg: %d | DO: %d\n", gas_analog_read, avg_gas, gas_digital_read);
+}
+
+
+/*################################################################*/
+/* Function for Blynk*/
+/*################################################################*/
+void connectToBlynk() {
+  unsigned long start = millis();
+  while (!Blynk.connected() && millis() - start < 10000)  // 10 seconds timeout
+  {
+    Blynk.run();
+    delay(1000);
+  }
+  if (Blynk.connected()) {
+    Serial.println("Blynk Connected!");
+  } else {
+    Serial.println("Blynk connection failed!");
+  }
+}
+
+/*################################################################*/
+/* Set Weight Widget code */
+/*################################################################*/
+BLYNK_WRITE(V1) {
+  set_weight = param.asInt();
+  digitalWrite(buzzer_pin, HIGH);
+  delay(100);
+  digitalWrite(buzzer_pin, LOW);
+  lcd.setCursor(0, 1);
+  lcd.print("               ");
+  Serial.print("Set Weight: ");
+  Serial.print(set_weight);
+  Serial.println("kg");
+}
+
+/*################################################################*/
+/* SETUP */
+/*################################################################*/
+void setup() {
+  Serial.begin(115200);
+
+  Serial.print("Connecting to Wifi SSID ");
+  Serial.print(ssid);
+  WiFi.begin(ssid, pass);
+  secured_client.setCACert(TELEGRAM_CERTIFICATE_ROOT);  // Add root certificate for api.telegram.org
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.print("\nWiFi connected. IP address: ");
+  Serial.println(WiFi.localIP());
+
+  /****************************************************************/
+  // Initialize blynk config
+  /****************************************************************/
+  Blynk.begin(auth, ssid, pass, server, port);
+
+  pinMode(lpg_digital_pin, INPUT);
+  pinMode(buzzer_pin, OUTPUT);
+  pinMode(led_pin, OUTPUT);
+
+  /****************************************************************/
+  //Initialize lcd
+  /****************************************************************/
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Gas Tank");
+  lcd.setCursor(0, 1);
+  lcd.print("Weight Detector");
+  delay(1000);
+  lcd.clear();
+  Serial.println("Starting...");
+
+  /****************************************************************/
+  //Initialize HX711 sensor
+  /****************************************************************/
+  LoadCell.begin();
+  LoadCell.setCalFactor(calibrationValue);  // Apply calibration
+
+  unsigned long stabilizingtime = 2000;
+  boolean _tare = true;
+  LoadCell.start(stabilizingtime, _tare);
+
+  if (LoadCell.getTareTimeoutFlag()) {
+    Serial.println("Timeout, check MCU>HX711 wiring and pin designations");
+    while (1)
+      ;
+  } else {
+    Serial.println("Startup is complete");
+  }
+  /****************************************************************/
+
+  bot.sendMessage(CHAT_ID, "System Ready. LPG Gas Tank Detection", "");
+  Serial.print("Current Calibration: ");
+  Serial.println(calibrationValue, 2);
+}
+
+/*################################################################*/
+/* LOOP */
+/*################################################################*/
+void loop() {
+  // Run Blynk
+  if (!Blynk.connected()) {
+    Serial.println("Blynk disconnected! Reconnecting...");
+    connectToBlynk();
+  }
+  Blynk.run();
+
+  /****************************************************************/
+  read_weight();
+  read_gas();
+}
